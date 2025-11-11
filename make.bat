6523986ebfe08c84e50dcdc0f0932b30dd62045d
Claude Müller <${GIT_AUTHOR_EMAIL}>
2025-11-11 08:54:21 +0100

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,1 +1,10 @@
- -DTINYFD_NO_POPEN
+clang ^
+    -target x86_64-pc-windows-gnu ^
+    -std=c11 -Wall -Wextra -DTINYFD_NO_POPEN -g ^
+    --sysroot="C:/msys64/mingw64" ^
+    -I"C:\lib\raylib-5.5-mingw_w64\include" ^
+    -I"lib" ^
+    src\*.c ^
+    -o bin\foodfight.exe ^
+    -L"C:\lib\raylib-5.5-mingw_w64\lib" ^
+    -lraylib -lopengl32 -lgdi32 -lwinmm
